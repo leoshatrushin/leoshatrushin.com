c655179fdc08301a47cf69a0badab257f12c27d7
leoshatrushin <u7302755@anu.edu.au>
2023-09-15 19:09:22 +1000

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,11 +11,20 @@ server.listen(3000, function listen() {
 	console.log('server started on port 3000');
 });
 
-/* Begin websocket */
+/* begin websocket */
 
 const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({server: server});
+
+process.on('SIGINT', () => {
+	wss.clients.forEach(function closeSocket(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutDownDB();
+	});
+});
 
 wss.on('connection', function connection(ws) {
 	const numClients = wss.clients.size;
@@ -26,6 +35,10 @@ wss.on('connection', function connection(ws) {
 	if (ws.readyState === ws.OPEN) {
 		ws.send('Welcome to my server');
 	}
+
+	db.run(`INSERT INTO visitors (count, time)
+		VALUES (${numClients}, datetime('now'))
+	`);
 
 	ws.on('close', function close() {
 		wss.broadcast(`Current visitors: ${numClients}`);
@@ -38,3 +51,29 @@ wss.broadcast = function broadcast(data) {
 		client.send(data);
 	});
 };
+
+/* end websocket */
+/* begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(function setupTables() {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`)
+});
+
+function getCounts() {
+	db.each("SELECT * FROM visitors", function logRow(err, row) {
+		console.log(row);
+	})
+}
+
+function shutDownDB() {
+	console.log("Shutting down db");
+	getCounts();
+	db.close();
+}
